ff26f84f1cc7fc4236bf1d9eaaac484619a191fc
ramcqueary <ramcqueary@gmail.com>
2022-06-06 16:32:49 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 from setuptools import setup
-import os
 
 
 setup(
   name='xtdotcomv1',
   version='1.0.0',
   author='Richard McQueary',
-  packages=['xtdotcomv1'],
+  packages=['xtdotcomv1',
+              'xtdotcomv1.utils',
+              'xtdotcomv1.public'],
   install_requires=['requests','pandas'],
   zip_safe=False
 )
